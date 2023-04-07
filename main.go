dfe057103615e93a3b13e4e4231d2e6d8b9a3f75
HomeSani <dfghjjhgfd311@gmail.com>
2023-04-07 23:44:27 +0300

    Add func for find max page index

--- main.go
+++ main.go
@@ -1,5 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func getMaxPageIndex() int {
+	maxPageIndex := 1
+
+	for true {
+		url := fmt.Sprintf("http://b2b.abktourism.kz/search_tour?TOWNFROMINC=10&STATEINC=6&CHECKIN_BEG=20230418&NIGHTS_FROM=7&CHECKIN_END=20230419&NIGHTS_TILL=9&ADULT=2&CURRENCY=4&CHILD=0&TOWNS_ANY=1&STARS_ANY=1&HOTELS_ANY=1&MEALS_ANY=1&PRICEPAGE=%d&DOLOAD=1", maxPageIndex)
+
+		res, err := http.Get(url)
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		defer res.Body.Close()
+
+		doc, err := goquery.NewDocumentFromReader(res.Body)
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		lastPageIndex, err := strconv.Atoi(doc.Find("span.page").Last().Text())
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		if maxPageIndex < lastPageIndex {
+			maxPageIndex = lastPageIndex
+		} else if maxPageIndex == lastPageIndex+1 {
+			break
+		}
+	}
+
+	return maxPageIndex
+}
+
 func main() {
-	var baseUrl string = "http://b2b.abktourism.kz/search_tour?TOWNFROMINC=10&STATEINC=6&CHECKIN_BEG=20230418&NIGHTS_FROM=7&CHECKIN_END=20230419&NIGHTS_TILL=9&ADULT=2&CURRENCY=4&CHILD=0&TOWNS_ANY=1&STARS_ANY=1&HOTELS_ANY=1&MEALS_ANY=1&PRICEPAGE=1&DOLOAD=1"
+	maxPageIndex := getMaxPageIndex()
+
+	fmt.Println(maxPageIndex)
 }
